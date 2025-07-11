1eea62e7b9f06a66baac4a69a2e1da24b546547b
Vishnu <arudra3ved@gmail.com>
2025-07-11 11:57:46 +0530

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Typewriter} from 'react-simple-typewriter';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../assets/GOPR0440.jpg'
+import profileImage from '../assets/GOPR0440.JPG'
 
 
 export default function About() {
